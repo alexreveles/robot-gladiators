79e4c3ce4c058a4b3f30baf6d5f31eeb8c9c6894
alex <alexreveles@yahooo.com>
2020-12-21 15:59:19 -0800

    Robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,28 @@
-window.alert("This is an alert! JavaScript is running!");
+/* This adds a window alert message */
+// window.alert("Time To Kick ASS!!!");
+/** End alert here */
+
+// this creates a function named "fight"
+
+// function fight() {
+//     window.alert("The fight has begun!");
+//   }
+//   fight();
+ // This line above is needed to call the function above in roder to work //
+
+//   fight();
+
+   // This is a prompt to allow to name your robot "user input" //
+
+  //window.prompt("What is your robot's name?");
+
+
+   // This allows to capture and store data the response from window prompt //
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
